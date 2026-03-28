1c21de77b2b728530c172211ba9e94c507cfae2a
lotoisrael <info@lotoisrael.com>
2026-03-28 21:30:19 +0300

    Update email.js

--- pages/api/email.js
+++ pages/api/email.js
@@ -87,7 +87,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: 'LotoIsrael <noreply@lotoisrael.com>',
+        from: 'LotoIsrael <Info@lotoisrael.com>',
         to: [to],
         subject,
         html,
